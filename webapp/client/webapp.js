1bae31a41bb31e54837680290bcc78f62c2b0d94
martin.ku <martin79831@gmail.com>
2014-06-06 20:59:48 +0800

    Add marker cluster.

--- webapp/client/webapp.js
+++ webapp/client/webapp.js
@@ -1,35 +1,33 @@
-var map;
 var taiwan;
 
+var map;
+var map_options;
+
+var marker_cluster;
+var marker_cluster_options = { gridSize: 50, maxZoom: 15 };
+
 Template.map_canvas.rendered = function () {
-  taiwan = new google.maps.LatLng(23.5989353, 121.0173534);
-  google.maps.event.addDomListener(window, 'load', initialize);
+  initialize_map();
 
   Meteor.call('get_locations', function (error, locations) {
-    if (error) {
-      console.log(error);
-    } else {
-      for (var i = 0; i < locations.length; i++) {
-        var position = new google.maps.LatLng(locations[i].lat,
-                                              locations[i].lng);
-        add_marker(position);
-      }
-    }
+    add_markers(locations);
   });
 };
 
 // initialize google map
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: taiwan
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+function initialize_map() {
+  taiwan = new google.maps.LatLng(23.5989353, 121.0173534);
+  map_options = { zoom: 8, center: taiwan };
+  map = new google.maps.Map(document.getElementById('map-canvas'), map_options);
 }
 
-function add_marker(position) {
-  var marker = new google.maps.Marker({
-    position: position,
-    map: map,
-  });
+function add_markers(locations) {
+  var markers = [];
+  for (var i = 0; i < locations.length; i++) {
+    var position = new google.maps.LatLng(locations[i].lat, locations[i].lng);
+    var marker = new google.maps.Marker({ position: position });
+    markers.push(marker);
+  }
+  //marker_cluster = new MarkerClusterer(map, markers, marker_cluster_options);
+  marker_cluster = new MarkerClusterer(map, markers);
 }
